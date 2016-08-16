7acbdfd331115a5a37b01584d74030133663d6dd
devchas <devchas@gmail.com>
2016-08-16 15:36:50 -0400

    set custom domain

--- settings.js
+++ settings.js
@@ -1,4 +1,4 @@
-exports.url = 'http://localhost:3090';
+exports.url = 'https://tony-opt-in.herokuapp.com/';
 exports.senderEmail = "hello@tonyblank.com";
 exports.senderName = "Tony Blank";
 exports.listID = 348282;
